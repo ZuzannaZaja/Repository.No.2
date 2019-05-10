2156db221ff02ce598e991fe37c9196f54332e42
ZuzannaZaja <zuzannazaja@gmail.com>
2019-05-10 11:20:28 +0200

    commit_zz-baza-kalendarz

--- app/src/main/java/com/example/app/DatePicker.java
+++ app/src/main/java/com/example/app/DatePicker.java
@@ -3,19 +3,37 @@ package com.example.app;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CalendarView;
 import android.widget.Toast;
 
 //import com.squareup.timessquare.CalendarPickerView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.text.DateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DatePicker extends AppCompatActivity {
 
 
     private CalendarView calendar;
+    private static String URL_LOGIN="http://192.168.0.38/calendar/getDay.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,11 +48,95 @@ public class DatePicker extends AppCompatActivity {
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange( CalendarView view, int year, int month, int dayOfMonth) {
-                Intent intent = new Intent(DatePicker.this, DayActivity.class);
-                startActivity(intent);
+                //Date date = new GregorianCalendar(year, month, dayOfMonth).getGregorianChange();
+
+                getDay(year, month+1, dayOfMonth);
+                /*Intent intent = new Intent(DatePicker.this, DayActivity.class);
+                intent.putExtra("year", year);
+                intent.putExtra("month", month);
+                intent.putExtra("dayOfMonth", dayOfMonth);
+                startActivity(intent);*/
+
+
+
             }
         });
 
-        
+
+    }
+
+    private void getDay(final int year,final int month,final int dayOfMonth){
+        final String date = dayOfMonth+"-"+month+"-"+year;
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_LOGIN,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);//format odpowiedzi
+                            String success = jsonObject.getString("success");
+
+
+                            if(success.equals("1")){
+
+                                ArrayList<String> hours = new ArrayList<>();
+                                hours.add(jsonObject.getString("hour9"));
+                                hours.add(jsonObject.getString("hour10"));
+                                hours.add(jsonObject.getString("hour11"));
+                                hours.add(jsonObject.getString("hour12"));
+                                hours.add(jsonObject.getString("hour13"));
+                                hours.add(jsonObject.getString("hour14"));
+                                hours.add(jsonObject.getString("hour15"));
+                                hours.add(jsonObject.getString("hour16"));
+
+                                //displaying all paramiters on login screen
+                                Toast.makeText(DatePicker.this,
+                                        "Success Login. \nHello "
+                                        , Toast.LENGTH_SHORT)
+                                        .show();
+                                Intent intent = new Intent(DatePicker.this, DayActivity.class);
+                                intent.putExtra("hours", hours);
+                                intent.putExtra("year", year);
+                                intent.putExtra("month", month);
+                                intent.putExtra("dayOfMonth", dayOfMonth);
+
+                                startActivity(intent);
+
+
+                                //}
+                            }else{
+                                Toast.makeText(DatePicker.this,
+                                        "Login Error \nWrong Email or Password"
+
+                                        , Toast.LENGTH_SHORT)
+                                        .show();
+                            }
+                        }catch(JSONException e) {
+
+                            Toast.makeText(DatePicker.this, "Error JSON "+e.toString(), Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Toast.makeText(DatePicker.this, "Error"+error.toString(), Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("date", date);
+
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
     }
 }
--- app/src/main/java/com/example/app/DayActivity.java
+++ app/src/main/java/com/example/app/DayActivity.java
@@ -1,13 +1,67 @@
 package com.example.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class DayActivity extends AppCompatActivity {
+    private TextView header;
+    private EditText a,b,c,d,e,f,g,h;
+    private ArrayList<EditText> lista = new ArrayList<>();
+    private Button btn_confirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day);
+        header = findViewById(R.id.textViewHeader);
+        btn_confirm = findViewById(R.id.textViewFooter);
+
+        a = findViewById(R.id.view9);
+        b = findViewById(R.id.view10);
+        c = findViewById(R.id.view11);
+        d = findViewById(R.id.view12);
+        e = findViewById(R.id.view13);
+        f = findViewById(R.id.view14);
+        g = findViewById(R.id.view15);
+        h = findViewById(R.id.view16);
+        lista.add(a);lista.add(b);lista.add(c);lista.add(d);lista.add(e);lista.add(f);lista.add(g);lista.add(h);
+        btn_confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for(EditText a : lista) {
+                    String string = a.getText().toString().trim();
+                    if(!string.isEmpty()){
+
+                        Toast.makeText(DayActivity.this, string ,Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+
+        Intent intent = getIntent();
+        int year = intent.getIntExtra("year", 0);
+        int month = intent.getIntExtra("month", 0);
+        int dayOfMonth = intent.getIntExtra("dayOfMonth", 0);
+        ArrayList<String> hours = intent.getStringArrayListExtra("hours");
+        header.setText(Integer.toString(dayOfMonth)+"-"+Integer.toString(month)+"-"+Integer.toString(year));
+        for(int i=0;i<hours.size();i++){
+            if(hours.get(i).equals("0")) {
+                lista.get(i).setText("");
+            }else{
+                lista.get(i).setHint("not available");
+                lista.get(i).setInputType(InputType.TYPE_NULL);
+            }
+        }
     }
 }
